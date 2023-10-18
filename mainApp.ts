75a31a38a69bded4cf391f7333782e207e77a060
Destiny onuiri <destinyonuiri@gmail.com>
2023-10-18 17:45:37 +0100

    do hard things

--- mainApp.ts
+++ mainApp.ts
@@ -1,10 +1,11 @@
 import cors from "cors"
 import express, {Application } from "express"
+import student from "./router/userRouter"
 
 export const mainApp =(app:Application)=>{
     app.use(express.json())
     app.use(cors())
-    
+    app.use("/api",student)
 
 app.get("/",()=>{
 try {
