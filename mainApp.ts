7af1cf6a251aa887f01c6dee71e8cb564cbe16dd
Destiny onuiri <destinyonuiri@gmail.com>
2023-10-18 13:53:12 +0100

    start

---
+++ mainApp.ts
@@ -0,0 +1,17 @@
+import cors from "cors"
+import express, {Application } from "express"
+
+export const mainApp =(app:Application)=>{
+    app.use(express.json())
+    app.use(cors())
+
+
+app.get("/",()=>{
+try {
+    console.log("welcome to our platform")
+} catch (error) {
+    console.log("error")
+}
+})
+}
+
