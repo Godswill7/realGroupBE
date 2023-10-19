f5ff2980b0a807a8bb6822b8766ab1ff31718366
Destiny onuiri <destinyonuiri@gmail.com>
2023-10-19 12:58:27 +0100

    express

--- mainApp.ts
+++ mainApp.ts
@@ -1,23 +1,7 @@
-import cors from "cors"
-import express, { Application, Request, Response } from "express"
-import student from "./router/userRouter"
-import { HTTP } from "./error/mainError"
+// import express,{Application,Request,Response} from "express"
 
-export const mainApp =(app:Application)=>{
-    app.use(express.json())
-    app.use(cors()) 
-    app.use("/api",student)
-    app.use(cors())
-    
+// const mainApp = ((app:Application)=>{
 
-app.get("/",(req:Request,res:Response)=>{
-try {
-    return res.status(HTTP.OK).json({
-        message: "Welcome",
-})
-} catch (error) {
-    console.log("error")
-}
-})
-}
+// })
 
+import express from "express"
